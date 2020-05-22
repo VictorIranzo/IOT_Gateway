147f8b58e1d4fef637d8f7cec67980f0ddee1a7e
Víctor Iranzo <viranzo96@gmail.com>
2020-05-23 01:00:06 +0200

    Configurando Ocelot.

---
+++ Gateway/Gateway/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Gateway
+{
+    using Microsoft.AspNetCore;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .ConfigureAppConfiguration((config) =>
+                {
+                    config.AddJsonFile("ocelot.json");
+                });
+    }
+}
